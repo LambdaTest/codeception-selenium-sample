14cffa0575b56fc18b87ddb150fc9fd65bac8954
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2021-10-08 15:47:17 +0000

    updated packages

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -4,7 +4,7 @@ class FirstCest
     public function frontpageWorks(AcceptanceTester $I)
     {
         sleep(5);
-        $I->amOnPage('/sample-todo-app');
+        $I->amOnPage('/sample-todo-app/');
         sleep(2);
         $I->checkOption('/html/body/div/div/div/ul/li[4]/input');
         sleep(2);
